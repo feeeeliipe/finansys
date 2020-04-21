d0f7fd089f37271ba35c4ba4dd4b488f3c8aa95e
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-04-20 22:07:47 -0300

    [FEAT]: Implementando componente de listagem das categorias

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      categories => {
+      this.categories = categories;
+    },
+    error => alert('Erro ao carregar a lista'))
+  }
+
+  deleteCategory(category: Category) {
+    const mustDelete = confirm('Deseja realmente deletar a categoria ?');
+    
+    if(mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => { 
+          this.categories = this.categories.filter(element => element != category);
+        }, 
+        error => {
+          alert('Erro ao excluir categoria')
+      })
+    }
+
   }
 
 }
