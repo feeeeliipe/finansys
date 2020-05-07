a07c89475eca174cc17e93032e0aa4e402476b17
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-05-07 12:52:59 -0300

    Criação do page header e refatoração dos componentes que possuem header

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface HeaderParams {
+  title: string,
+  btnClass: string,
+  btnLink: string,
+  btnText: string
+}
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+
+  @Input() headerParams: HeaderParams;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
