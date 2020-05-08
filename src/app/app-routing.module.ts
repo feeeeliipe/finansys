676930ecc3e87c38118cfe21d33707044b2737b5
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-05-07 21:13:20 -0300

    Ajustando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/reports', pathMatch: 'full' },
   { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(mod => mod.CategoriesModule) },
   { path: 'entries',    loadChildren: () => import('./pages/entries/entries.module').then(mod => mod.EntriesModule) },
   { path: 'reports',    loadChildren: () => import('./pages/reports/reports.module').then(mod => mod.ReportsModule) }
