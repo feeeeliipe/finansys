fd08f45b13a82ae94789df59157fd5ac3757f6f8
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-05-12 18:17:41 -0300

    Criando componente de cadastro e edição de metas

--- src/app/pages/goals/goal-form/goal-form.component.ts
+++ src/app/pages/goals/goal-form/goal-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { GoalService } from '../shared/goals.service';
+import { Goal } from '../shared/goals.model';
+import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
-export class GoalFormComponent implements OnInit {
+export class GoalFormComponent extends BaseResourceFormComponent<Goal> {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(protected goalService: GoalService, protected injector: Injector) { 
+    super(injector, new Goal(), goalService, Goal.fromJson);
   }
 
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      description: [null, Validators.required],
+      initialDate: [null, Validators.required],
+      endDate: [null, Validators.required],
+      expectedAmount: [null, Validators.required],
+      installmentsQuantity: [null],
+      installmentsValue: [null]
+    });
+  }
+
+  protected creationPageTitle(): string {
+    return "Nova Meta";
+  }
+
+  protected editionPageTitle(): string {
+    let title = this.resource.description || '';
+    title = "Editando a meta: " + title;
+    return title;
+  }
 }
