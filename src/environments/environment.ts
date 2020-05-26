670526f9888e5b7b5dcc02d67b00b3bff4f1b8fb
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-05-25 22:16:55 -0300

    commit to deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3001',
+  base_url: 'https://fortune-backend.herokuapp.com',
   jwt_secret: 'ec6fca6739d7d0167a42fced7e3c9638'
 };
 
